1e80c3aab1b1126b316c518687c8ffe49518846a
Rajveer <yrajveer094@gmail.com>
2024-11-11 19:47:18 +0530

    Update post.js

--- post.js
+++ post.js
@@ -1,8 +1,8 @@
 const { webHook_url } = require("./settings.json");
-const { greenBright, redBright, grey, yellowBright } = require("chalk");
+const { greenBright, redBright, yellowBright,grey } = require("chalk");
 const ora = require("ora");
 const readline = require("readline").createInterface({ input: process.stdin, output: process.stdout });
-const fetch = require("node-fetch");  
+const fetch = require("node-fetch");
 
 readline.question(grey("[?] Do you wish to post these links? (Y/N) "), async (answr) => {
     if (answr.toLowerCase() === "y" || answr.toLowerCase() === "yes") {
@@ -11,7 +11,6 @@ readline.question(grey("[?] Do you wish to post these links? (Y/N) "), async (an
         process.exit(1);
     }
 });
-
 
 async function Post() {
     const spinner = ora("Preparing to post").start();
@@ -29,10 +28,28 @@ async function Post() {
                     method: "POST",
                     headers: { "Content-Type": "application/json" },
                     body: JSON.stringify({ content: link })
-                });
+                    });
 
                 if (response.ok) {
                     spinner.succeed(greenBright(`[${index}] Link Posted: ${yellowBright(link)}`));
+                } else if (response.status === 429) {
+                    const errorText = await response.json();
+                    const retryAfter = errorText.retry_after;
+                    spinner.warn(redBright(`[${index}] Rate limit hit | Retrying in ${retryAfter+10} seconds...`));
+                    await new Promise(resolve => setTimeout(resolve, (retryAfter+10) * 1000));
+
+                    const retryResponse = await fetch(webHook_url, {
+                        method: "POST",
+                        headers: { "Content-Type": "application/json" },
+                        body: JSON.stringify({content: link})
+                    });
+
+                    if (retryResponse.ok) {
+                        spinner.succeed(greenBright(`[${index}] Link Retried and Posted: ${yellowBright(link)}`));
+                    } else {
+                        const retryErrorText = await retryResponse.text();
+                        spinner.fail(redBright(`[${index}] Link failed to post after retry | ${retryErrorText}`));
+                    }
                 } else {
                     const errorText = await response.text();
                     spinner.fail(redBright(`[${index}] Link failed to post | ${errorText}`));
